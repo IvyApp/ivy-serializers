c54219e24216abcd8c5ac699ce58862bbbe01bd0
Dray Lacy <dray@envylabs.com>
2015-05-05 13:45:52 -0400

    Remove railtie. I plan to extract this to a separate gem.

--- lib/ivy/serializers.rb
+++ lib/ivy/serializers.rb
@@ -2,4 +2,3 @@ require 'ivy/serializers/documents'
 require 'ivy/serializers/formats'
 require 'ivy/serializers/serializer'
 require 'ivy/serializers/version'
-require 'ivy/serializers/railtie' if defined?(::Rails)
